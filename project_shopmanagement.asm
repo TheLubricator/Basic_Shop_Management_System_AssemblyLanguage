6b9777a9c5bd593d10d183d253dbeb31b72f9b6f
Faiyaz Zaman <51377374+TheLubricator@users.noreply.github.com>
2025-01-01 23:10:50 +0600

    project finished, will test more

--- project_shopmanagement.asm
+++ project_shopmanagement.asm
@@ -1,46 +1,232 @@
 .MODEL SMALL
-DecimalPrinter macro var1 ;limit 2 hex bytes, 255 decimal
-    mov al,var1
-    mov ah,0
-    mov bl,100
-    div bl
-    mov ch,ah
-    mov cl,al
-    mov dl,cl
+DecimalPrinter macro var1 ;limit 4 hex bytes, 255 decimal
+    mov ax,var1
+    
+    mov bx,10000 
+    mov dx,0
+    div bx
+    mov cx,dx
+    mov dl,al
     add dl,30h
     mov ah,2
     int 21h 
-    mov ax,0
-    mov al,ch
-    mov bl,10
-    div bl
+
+    mov ax,cx
+    mov bx,1000  
+    mov dx,0
+    div bx
     
-    mov ch,ah
-    mov cl,al
-    mov dl,cl
+    mov cx,dx
+    mov dl,al
     add dl,30h
     mov ah,2
     int 21h
     
-    mov dl,ch
+    mov ax,cx
+    mov bx,100  
+    mov dx,0
+    div bx
+    
+    mov cx,dx
+    mov dl,al
     add dl,30h
     mov ah,2
     int 21h 
+    
+    mov ax,cx
+    mov bx,10  
+    mov dx,0
+    div bx
+    
+    mov cx,dx
+    mov dl,al
+    add dl,30h
+    mov ah,2
+    int 21h 
+    
+    mov dl,cl
+    add dl,30h
+    mov ah,2
+    int 21h 
+    
+    
     endm
 
 StockAdder macro var1
-    mov bl, var1
-    sub cl,30h
-    add bl,cl
-    mov var1,bl
+    mov bx, var1
+    add bx,cx
+    mov var1,bx
     endm
+
+SellItemProcess1 macro price,stock, cash_register, revenue
+    mov bx, stock
+    cmp bx,0
+    je Itemdepleted1 
+    cmp cx,bx
+    jg StockExceeded1 
+    
+    sub bx,cx
+    mov stock,bx
+    mov bx, price  
+    mov ax,0
+    priceloop1: 
+    add ax,bx
+    loop priceloop1
+    
+    mov bx, cash_register
+    add bx,ax
+    mov cash_register,bx
+    mov bx,revenue
+    add bx,ax
+    mov revenue,bx 
+    jmp endpoint1
+   
+    
+    StockExceeded1:
+    mov ah,9
+    lea dx, stockexceed
+    int 21h 
+    call newlinecursorzero
+    mov ah,9
+    lea dx, presskeytocontinue
+    int 21h 
+    mov ah,1
+    int 21h
+    jmp SellItemPage 
+    
+    Itemdepleted1:
+    mov ah,9
+    lea dx, itemdeplete
+    int 21h 
+    call newlinecursorzero
+    mov ah,9
+    lea dx, presskeytocontinue
+    int 21h 
+    mov ah,1
+    int 21h
+    jmp SellItemPage
+    
+    endpoint1:
+    mov cx,ax
+    endm
+
+SellItemProcess2 macro price,stock, cash_register, revenue
+    mov bx, stock
+    cmp bx,0
+    je Itemdepleted2 
+    cmp cx,bx
+    jg StockExceeded2 
+    
+    sub bx,cx
+    mov stock,bx
+    mov bx, price  
+    mov ax,0
+    priceloop2: 
+    add ax,bx
+    loop priceloop2
+    
+    mov bx, cash_register
+    add bx,ax
+    mov cash_register,bx
+    mov bx,revenue
+    add bx,ax
+    mov revenue,bx 
+    jmp endpoint2
+   
+    
+    StockExceeded2:
+    mov ah,9
+    lea dx, stockexceed
+    int 21h 
+    call newlinecursorzero
+    mov ah,9
+    lea dx, presskeytocontinue
+    int 21h 
+    mov ah,1
+    int 21h
+    jmp SellItemPage 
+    
+    Itemdepleted2:
+    mov ah,9
+    lea dx, itemdeplete
+    int 21h 
+    call newlinecursorzero
+    mov ah,9
+    lea dx, presskeytocontinue
+    int 21h 
+    mov ah,1
+    int 21h
+    jmp SellItemPage
+    
+    endpoint2:
+    mov cx,ax
+    endm    
+ 
+ 
+SellItemProcess3 macro price,stock, cash_register, revenue
+    mov bx, stock
+    cmp bx,0
+    je Itemdepleted3 
+    cmp cx,bx
+    jg StockExceeded3 
+    
+    sub bx,cx
+    mov stock,bx
+    mov bx, price  
+    mov ax,0
+    priceloop3: 
+    add ax,bx
+    loop priceloop3
+    
+    mov bx, cash_register
+    add bx,ax
+    mov cash_register,bx
+    mov bx,revenue
+    add bx,ax
+    mov revenue,bx 
+    jmp endpoint3
+   
+    
+    StockExceeded3:
+    mov ah,9
+    lea dx, stockexceed
+    int 21h 
+    call newlinecursorzero
+    mov ah,9
+    lea dx, presskeytocontinue
+    int 21h 
+    mov ah,1
+    int 21h
+    jmp SellItemPage 
+    
+    Itemdepleted3:
+    mov ah,9
+    lea dx, itemdeplete
+    int 21h 
+    call newlinecursorzero
+    mov ah,9
+    lea dx, presskeytocontinue
+    int 21h 
+    mov ah,1
+    int 21h
+    jmp SellItemPage
+    
+    endpoint3:
+    mov cx,ax
+    endm 
+ 
  
 .STACK 100H
 
 .DATA   
 LoginWelcomeMessage db "Welcome to Shop X! Please Login to continue.$"
-spacer db " $"
-dollarendermsg db " dollars$"
+spacer db " $" 
+arrowpointer db " ==> $" 
+one db "1.) $" 
+two db "2.) $"  
+three db "3.) $"
+dollarendermsg db " dollars$" 
+dollarendermsg1 db " dollars from customer$"
 userid db 04Dh,61h,73h,74h,65h,06Dh,61h        
 password db 35h,34h,33h,61h,62h,63h,64h
 userIDinput db 7 dup(?)
@@ -50,7 +236,8 @@ passinputmsg db "Password: $"
 InvalidCredentials db "Invalid Credentials! Press any key to go back to login screen. $" 
 welcome db "Welcome Mastema! These are your options$"
 success db "Success!$"
-invalidnumber db "Please enter a valid number!$"
+invalidnumber db "Please enter a number between 1 and 25!$"  
+invalidnumber1 db "Please enter a number between 1 and 50!$"
 presskeytocontinue db "Press a key to continue: $"      
 ;main menu strings  
 PressHashToMainMenu db "Press # to return to main menu: $"
@@ -58,27 +245,56 @@ SelectorHash db "Select Item or press # to return to main menu: $"
 PressNumberToContinue db "Pick an option to continue: $"
 main_menu_cash_register_msg db "Amount in cash_register: $" 
 main_menu_viewstock db "1.) View Stock$"   
-main_menu_AddStock db "2.) Add Stock$" 
+main_menu_AddStock db "2.) Add Stock$"
+main_menu_SellItem db "3.) Sell Products$" 
+main_menu_ViewRevenue db "4.) View Revenue$"
+main_menu_Logout db "5.) Logout$"
 
 ; View stock page string
 viewstockstr db "This page displays stock number for each item$" 
 
 
 ;AddStockPage
-addstockstr db "Add stock from this page (greater than 0)$" 
-enternumber db "Enter a valid number between 1-9: $"
+addstockstr db "Add stock from this page (Between 1 and 25)$" 
+enternumber db "Enter a valid number between 1-25: $"
+
+
+;SellItemPage 
+sellitemstr db "Pick an item and quantity to sell(nonzero,50 per customer max)$"
+enternumber1 db "Enter a valid number between 1-50: $"
+stockexceed db "Number of items exceed stock amount. Restock or pick another item $" 
+itemdeplete db "Item unavailable. Please select another item $"
+sellsuccess db "Valid purchase! Please collect $"   
+
+;ViewRevenuePage
+ViewRevenuestr db "This page displays revenue generated from each item$" 
+
+;Logoout
+Logoutstr db "You will be logged out and won't be able to continue store activities. Continue?[y/anything else]: $"
+
 
 ;numerical data
-cash_register db 56 
-item_1_stock db 1  
-item_2_stock db 2
-item_3_stock db 3
+cash_register dw 0 
+item_1_stock dw 1  
+item_2_stock dw 2
+item_3_stock dw 3  
+
+item_1_price dw 25   
+item_2_price dw 50
+item_3_price dw 73
+
+item_1_revenue dw 0
+item_2_revenue dw 0
+item_3_revenue dw 0  
+
+temp db 0
+
 
 
 ;item names
-item1_name db "Item 1: $"
-item2_name db "Item 2: $"  
-item3_name db "Item 3: $"
+item1_name db "Item 1$"
+item2_name db "Item 2$"  
+item3_name db "Item 3$"
 
 
 
@@ -136,7 +352,7 @@ clearscr proc
      
     ret
     clearscr endp  
-newlinecursorzero proc
+newlinecursorzero proc  ;newline carriage return given a procedure form for modularity
     mov ah,2
     mov dl,0ah ;new line
     int 21h   
@@ -260,7 +476,7 @@ jmp bigbang
 
 CorrectAuth: 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;Main menu start
+;;;;;;;;;;;;;;;;;;;;;;;;;Main menu start  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 MainMenu:
 call clearscr
 call hashloop
@@ -284,7 +500,22 @@ int 21h
 
 call newlinecursorzero
 mov ah,9
-lea dx, main_menu_AddStock
+lea dx, main_menu_AddStock  
+int 21h
+
+
+call newlinecursorzero
+mov ah,9
+lea dx, main_menu_SellItem   
+int 21h 
+
+call newlinecursorzero
+mov ah,9
+lea dx, main_menu_ViewRevenue   
+int 21h
+call newlinecursorzero
+mov ah,9
+lea dx, main_menu_Logout   
 int 21h 
  
  
@@ -301,7 +532,15 @@ int 21h
 cmp al,31h
 je ViewStockPage
 cmp al,32h
-je AddStockPage
+je AddStockPage 
+
+cmp al,33h
+je SellItemPage  
+
+cmp al,34h
+je ViewRevenue
+cmp al,35h
+je Logout
 
 
 
@@ -309,7 +548,7 @@ jmp MainMenu
  
  
  
- ;;;;;;;;;;;;;;View stock Page
+ ;;;;;;;;;;;;;;View stock Page ;;;;;;;;;;;;;;;;;;
 ViewStockPage:
 call clearscr
 call hashloop
@@ -317,21 +556,32 @@ call movepointertomiddle
 mov ah,9
 lea dx,viewstockstr 
 int 21h
+call newlinecursorzero 
 call newlinecursorzero
 mov ah,9
 lea dx,item1_name
+int 21h  
+mov ah,9
+lea dx,arrowpointer
 int 21h
+
 DecimalPrinter item_1_stock    
 
 call newlinecursorzero
 mov ah,9
 lea dx,item2_name
+int 21h 
+mov ah,9
+lea dx,arrowpointer
 int 21h
 DecimalPrinter item_2_stock
 
 call newlinecursorzero
 mov ah,9
 lea dx,item3_name
+int 21h   
+mov ah,9
+lea dx,arrowpointer
 int 21h
 DecimalPrinter item_3_stock
 
@@ -342,9 +592,12 @@ lea dx, PressHashToMainMenu
 int 21h
 
 mov ah,1
-int 21h
+int 21h 
 
-jmp MainMenu
+cmp al,23h
+je MainMenu
+
+jmp ViewStockPage
 
 
  ;;;;;;;;;;;;;;Add Stock Page ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -355,18 +608,28 @@ call movepointertomiddle
 mov ah,9
 lea dx, addstockstr 
 int 21h
+call newlinecursorzero    
 call newlinecursorzero
+mov ah,9
+lea dx,one
+int 21h 
 mov ah,9
 lea dx,item1_name
 int 21h 
 
-call newlinecursorzero
+call newlinecursorzero   
+mov ah,9
+lea dx,two
+int 21h 
 mov ah,9
 lea dx,item2_name
 int 21h
 
 
-call newlinecursorzero
+call newlinecursorzero  
+mov ah,9
+lea dx,three
+int 21h 
 mov ah,9
 lea dx,item3_name
 int 21h
@@ -406,12 +669,26 @@ int 21h
 
 call newlinecursorzero
 mov ah,1
+int 21h  
+
+
+
+sub al,30h
+mov bl,10
+mul bl       ;2 digit deccimal input
+mov cl,al
+mov ah,1
 int 21h
-cmp al,30h
+sub al,30h
+add cl,al 
+mov ch,0
+
+
+cmp cl,0
 jle InputErrorAddStock1
-cmp al,3Ah
+cmp cl,26
 jge InputErrorAddStock1 
-mov cl,al 
+
 call newlinecursorzero
 mov ah,9
 lea dx, success
@@ -455,12 +732,22 @@ int 21h
 
 call newlinecursorzero
 mov ah,1
+int 21h 
+
+sub al,30h
+mov bl,10
+mul bl
+mov cl,al
+mov ah,1
 int 21h
-cmp al,30h
+sub al,30h
+add cl,al 
+mov ch,0
+
+cmp cl,0
 jle InputErrorAddStock2
-cmp al,3Ah
-jge InputErrorAddStock2 
-mov cl,al 
+cmp cl,26
+jge InputErrorAddStock2  
 call newlinecursorzero
 mov ah,9
 lea dx, success
@@ -503,12 +790,23 @@ int 21h
 
 call newlinecursorzero
 mov ah,1
+int 21h 
+
+sub al,30h
+mov bl,10
+mul bl
+mov cl,al
+mov ah,1
 int 21h
-cmp al,30h
+sub al,30h
+add cl,al
+mov ch,0
+
+cmp cl,0
 jle InputErrorAddStock3
-cmp al,3Ah
+cmp cl,26
 jge InputErrorAddStock3 
-mov cl,al 
+ 
 call newlinecursorzero
 mov ah,9
 lea dx, success
@@ -541,10 +839,404 @@ jmp AddItem3
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;END OF ADD STOCK;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+ ;;;;;;;;;;;START OF SELL ITEM;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+SellItemPage:   
+
+call clearscr
+call hashloop
+
+mov ah,9
+lea dx, sellitemstr 
+int 21h
+call newlinecursorzero    
+call newlinecursorzero
+mov ah,9
+lea dx,one
+int 21h 
+mov ah,9
+lea dx,item1_name
+int 21h
+
+mov ah,9
+lea dx,spacer
+int 21h  
+
+mov ah,9
+lea dx,arrowpointer
+int 21h 
+
+DecimalPrinter item_1_price
+mov ah,9
+lea dx,dollarendermsg
+int 21h
+
+call newlinecursorzero   
+mov ah,9
+lea dx,two
+int 21h 
+mov ah,9
+lea dx,item2_name
+int 21h
+
+mov ah,9
+lea dx,spacer
+int 21h  
+
+mov ah,9
+lea dx,arrowpointer
+int 21h 
+
+DecimalPrinter item_2_price
+mov ah,9
+lea dx,dollarendermsg
+int 21h
+
+
+call newlinecursorzero  
+mov ah,9
+lea dx,three
+int 21h 
+mov ah,9
+lea dx,item3_name
+int 21h
+
+
+mov ah,9
+lea dx,spacer
+int 21h  
+
+mov ah,9
+lea dx,arrowpointer
+int 21h 
+
+DecimalPrinter item_3_price
+mov ah,9
+lea dx,dollarendermsg
+int 21h
+
+
+call newlinecursorzero    
+call newlinecursorzero
+call hashloop   
+
+mov ah,9
+lea dx, SelectorHash
+int 21h
+
+
+mov ah,1
+int 21h
+cmp al, 31h
+je SellItem1   
+
+cmp al, 32h       ;;;;Sub menu redirection for 3 items
+je SellItem2
+
+cmp al, 33h
+je SellItem3
+
+cmp al, 23h
+je MainMenu  
+
+jmp SellItemPage ;any invalid input will refresh page  
 
 
 
 
+
+ ;sell item 1 logic
+SellItem1:    
+call clearscr
+call hashloop
+
+mov ah,9
+lea dx, enternumber1
+int 21h 
+
+call newlinecursorzero
+mov ah,1
+int 21h  
+
+
+
+sub al,30h
+mov bl,10
+mul bl       ;2 digit deccimal input
+mov cl,al
+mov ah,1
+int 21h
+sub al,30h
+add cl,al 
+mov ch,0
+
+
+cmp cl,0
+jle InputErrorSellItem1
+cmp cl,51
+jge InputErrorSellItem1 
+
+call newlinecursorzero
+
+
+ 
+SellItemProcess1 item_1_price,item_1_stock,cash_register,item_1_revenue    
+call newlinecursorzero
+mov ah,9
+lea dx, sellsuccess
+int 21h   
+
+DecimalPrinter cx
+mov ah,9
+lea dx, dollarendermsg1
+int 21h 
+
+call newlinecursorzero
+call hashloop 
+mov ah,9
+lea dx, presskeytocontinue
+int 21h
+mov ah,1
+int 21h
+jmp SellItemPage   
+
+
+InputErrorSellITem1:
+call newlinecursorzero
+mov ah,9
+lea dx, invalidnumber1
+int 21h
+call newlinecursorzero 
+mov ah,9
+lea dx, presskeytocontinue
+int 21h  
+mov ah,1
+int 21h
+jmp SellItem1   
+
+
+
+
+ ;sell item 2 logic
+SellItem2:    
+call clearscr
+call hashloop
+
+mov ah,9
+lea dx, enternumber1
+int 21h 
+
+call newlinecursorzero
+mov ah,1
+int 21h  
+
+
+
+sub al,30h
+mov bl,10
+mul bl       ;2 digit deccimal input
+mov cl,al
+mov ah,1
+int 21h
+sub al,30h
+add cl,al 
+mov ch,0
+
+
+cmp cl,0
+jle InputErrorSellItem2
+cmp cl,51
+jge InputErrorSellItem2 
+
+call newlinecursorzero
+
+
+ 
+SellItemProcess2 item_2_price,item_2_stock,cash_register,item_2_revenue    
+call newlinecursorzero
+mov ah,9
+lea dx, sellsuccess
+int 21h   
+
+DecimalPrinter cx
+mov ah,9
+lea dx, dollarendermsg1
+int 21h 
+
+call newlinecursorzero
+call hashloop 
+mov ah,9
+lea dx, presskeytocontinue
+int 21h
+mov ah,1
+int 21h
+jmp SellItemPage   
+
+
+InputErrorSellITem2:
+call newlinecursorzero
+mov ah,9
+lea dx, invalidnumber1
+int 21h
+call newlinecursorzero 
+mov ah,9
+lea dx, presskeytocontinue
+int 21h  
+mov ah,1
+int 21h
+jmp SellItem2  
+
+
+
+ ;sell item 3 logic
+SellItem3:    
+call clearscr
+call hashloop
+
+mov ah,9
+lea dx, enternumber1
+int 21h 
+
+call newlinecursorzero
+mov ah,1
+int 21h  
+
+
+
+sub al,30h
+mov bl,10
+mul bl       ;2 digit deccimal input
+mov cl,al
+mov ah,1
+int 21h
+sub al,30h
+add cl,al 
+mov ch,0
+
+
+cmp cl,0
+jle InputErrorSellItem3
+cmp cl,51
+jge InputErrorSellItem3 
+
+call newlinecursorzero
+
+
+ 
+SellItemProcess3 item_3_price,item_3_stock,cash_register,item_3_revenue    
+call newlinecursorzero
+mov ah,9
+lea dx, sellsuccess
+int 21h   
+
+DecimalPrinter cx
+mov ah,9
+lea dx, dollarendermsg1
+int 21h 
+
+call newlinecursorzero
+call hashloop 
+mov ah,9
+lea dx, presskeytocontinue
+int 21h
+mov ah,1
+int 21h
+jmp SellItemPage   
+
+
+InputErrorSellITem3:
+call newlinecursorzero
+mov ah,9
+lea dx, invalidnumber1
+int 21h
+call newlinecursorzero 
+mov ah,9
+lea dx, presskeytocontinue
+int 21h  
+mov ah,1
+int 21h
+jmp SellItem3 
+
+;;;;;;;;;END OF SELL ITEM;;;;;;;;;;;;;;;;;
+
+
+
+  ;;;;;START VIEW REVENUE;;;;;;;;;;;;;;;;;;;;;;;
+  
+ViewRevenue:
+call clearscr
+call hashloop
+call movepointertomiddle
+mov ah,9
+lea dx,ViewRevenuestr 
+int 21h
+call newlinecursorzero 
+call newlinecursorzero
+mov ah,9
+lea dx,item1_name
+int 21h  
+mov ah,9
+lea dx,arrowpointer
+int 21h
+
+DecimalPrinter item_1_revenue
+mov ah,9
+lea dx,dollarendermsg
+int 21h     
+
+call newlinecursorzero
+mov ah,9
+lea dx,item2_name
+int 21h 
+mov ah,9
+lea dx,arrowpointer
+int 21h
+DecimalPrinter item_2_revenue 
+mov ah,9
+lea dx,dollarendermsg
+int 21h
+
+call newlinecursorzero
+mov ah,9
+lea dx,item3_name
+int 21h   
+mov ah,9
+lea dx,arrowpointer
+int 21h
+DecimalPrinter item_3_revenue
+mov ah,9
+lea dx,dollarendermsg
+int 21h
+
+call newlinecursorzero
+call hashloop
+mov ah,9
+lea dx, PressHashToMainMenu
+int 21h
+
+mov ah,1
+int 21h
+cmp al,23h
+je MainMenu
+jmp ViewRevenue
+
+
+Logout:
+call clearscr
+call newlinecursorzero
+mov ah,9
+lea dx,Logoutstr
+int 21h
+mov ah,1
+int 21h
+
+mov temp,al
+call clearscr 
+mov cl,temp
+cmp cl,79h
+je bigbang
+
+jmp MainMenu
 
 
 

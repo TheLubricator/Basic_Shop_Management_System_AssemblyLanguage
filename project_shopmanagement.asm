85a7cff10158f4de25f62f8394e24667c1e499f2
Faiyaz Zaman <51377374+TheLubricator@users.noreply.github.com>
2025-01-01 17:07:35 +0600

    Update project_shopmanagement.asm

--- project_shopmanagement.asm
+++ project_shopmanagement.asm
@@ -27,6 +27,13 @@ DecimalPrinter macro var1 ;limit 2 hex bytes, 255 decimal
     mov ah,2
     int 21h 
     endm
+
+StockAdder macro var1
+    mov bl, var1
+    sub cl,30h
+    add bl,cl
+    mov var1,bl
+    endm
  
 .STACK 100H
 
@@ -41,25 +48,31 @@ IDinputmsg db "User ID: $"
 passwordinput db 7 dup(?)
 passinputmsg db "Password: $"
 InvalidCredentials db "Invalid Credentials! Press any key to go back to login screen. $" 
-welcome db "Welcome Mastema! These are your options$"      
+welcome db "Welcome Mastema! These are your options$"
+success db "Success!$"
+invalidnumber db "Please enter a valid number!$"
+presskeytocontinue db "Press a key to continue: $"      
 ;main menu strings  
 PressHashToMainMenu db "Press # to return to main menu: $"
+SelectorHash db "Select Item or press # to return to main menu: $"
 PressNumberToContinue db "Pick an option to continue: $"
 main_menu_cash_register_msg db "Amount in cash_register: $" 
 main_menu_viewstock db "1.) View Stock$"   
-main_menu_AddStock db "1.) Add Stock$" 
+main_menu_AddStock db "2.) Add Stock$" 
 
 ; View stock page string
 viewstockstr db "This page displays stock number for each item$" 
 
 
-;
+;AddStockPage
+addstockstr db "Add stock from this page (greater than 0)$" 
+enternumber db "Enter a valid number between 1-9: $"
 
 ;numerical data
 cash_register db 56 
-item_1_stock db 254  
-item_2_stock db 153
-item_3_stock db 123
+item_1_stock db 1  
+item_2_stock db 2
+item_3_stock db 3
 
 
 ;item names
@@ -92,6 +105,7 @@ hashloop proc   ;produces 70 hash symbols then two spaces, decoration purpose
     int 21h
     ret
     hashloop endp 
+
 movepointertomiddle proc     ; paired with hash, move pointer to middleish to print header msg, decoration PURPOSES
     mov cx,14
     pointerloop:
@@ -269,6 +283,12 @@ lea dx, main_menu_viewstock
 int 21h  
 
 call newlinecursorzero
+mov ah,9
+lea dx, main_menu_AddStock
+int 21h 
+ 
+ 
+call newlinecursorzero
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Main menu option end
 call hashloop  
 mov ah,9
@@ -280,6 +300,11 @@ int 21h
 
 cmp al,31h
 je ViewStockPage
+cmp al,32h
+je AddStockPage
+
+
+
 jmp MainMenu
  
  
@@ -320,6 +345,204 @@ mov ah,1
 int 21h
 
 jmp MainMenu
+
+
+ ;;;;;;;;;;;;;;Add Stock Page ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+AddStockPage:
+call clearscr
+call hashloop
+call movepointertomiddle
+mov ah,9
+lea dx, addstockstr 
+int 21h
+call newlinecursorzero
+mov ah,9
+lea dx,item1_name
+int 21h 
+
+call newlinecursorzero
+mov ah,9
+lea dx,item2_name
+int 21h
+
+
+call newlinecursorzero
+mov ah,9
+lea dx,item3_name
+int 21h
+
+
+call newlinecursorzero
+call hashloop
+mov ah,9
+lea dx, SelectorHash
+int 21h
+
+mov ah,1
+int 21h
+cmp al, 31h
+je AddItem1   
+
+cmp al, 32h       ;;;;Sub menu redirection for 3 items
+je AddItem2
+
+cmp al, 33h
+je AddItem3
+
+cmp al, 23h
+je MainMenu  
+
+jmp AddStockPage ;any invalid input will refresh page
+
+;;;;;;;;;
+;add stock logic for item 1
+AddItem1:  
+call clearscr
+call hashloop
+
+mov ah,9
+lea dx, enternumber
+int 21h 
+
+call newlinecursorzero
+mov ah,1
+int 21h
+cmp al,30h
+jle InputErrorAddStock1
+cmp al,3Ah
+jge InputErrorAddStock1 
+mov cl,al 
+call newlinecursorzero
+mov ah,9
+lea dx, success
+int 21h 
+ 
+call newlinecursorzero
+mov ah,9
+lea dx, presskeytocontinue
+int 21h 
+
+ 
+StockAdder item_1_stock
+mov ah,1
+int 21h
+jmp AddStockPage   
+
+InputErrorAddStock1:
+call newlinecursorzero
+mov ah,9
+lea dx, invalidnumber
+int 21h
+call newlinecursorzero 
+mov ah,9
+lea dx, presskeytocontinue
+int 21h  
+mov ah,1
+int 21h
+jmp AddItem1 
+
+
+
+;;;;;;;;;
+;add stock logic for item 2
+AddItem2:  
+call clearscr
+call hashloop
+
+mov ah,9
+lea dx, enternumber
+int 21h 
+
+call newlinecursorzero
+mov ah,1
+int 21h
+cmp al,30h
+jle InputErrorAddStock2
+cmp al,3Ah
+jge InputErrorAddStock2 
+mov cl,al 
+call newlinecursorzero
+mov ah,9
+lea dx, success
+int 21h 
+ 
+call newlinecursorzero
+mov ah,9
+lea dx, presskeytocontinue
+int 21h 
+
+ 
+StockAdder item_2_stock
+mov ah,1
+int 21h
+jmp AddStockPage   
+
+InputErrorAddStock2:
+call newlinecursorzero
+mov ah,9
+lea dx, invalidnumber
+int 21h
+call newlinecursorzero 
+mov ah,9
+lea dx, presskeytocontinue
+int 21h 
+mov ah,1
+int 21h
+jmp AddItem2
+
+
+;;;;;;;;;
+;add stock logic for item 3
+AddItem3:  
+call clearscr
+call hashloop
+
+mov ah,9
+lea dx, enternumber
+int 21h 
+
+call newlinecursorzero
+mov ah,1
+int 21h
+cmp al,30h
+jle InputErrorAddStock3
+cmp al,3Ah
+jge InputErrorAddStock3 
+mov cl,al 
+call newlinecursorzero
+mov ah,9
+lea dx, success
+int 21h 
+ 
+call newlinecursorzero
+mov ah,9
+lea dx, presskeytocontinue
+int 21h 
+
+ 
+StockAdder item_3_stock
+mov ah,1
+int 21h
+jmp AddStockPage   
+
+InputErrorAddStock3:
+call newlinecursorzero
+mov ah,9
+lea dx, invalidnumber
+int 21h
+call newlinecursorzero 
+mov ah,9
+lea dx, presskeytocontinue
+int 21h 
+mov ah,1
+int 21h
+jmp AddItem3     
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;END OF ADD STOCK;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
 
 
 
